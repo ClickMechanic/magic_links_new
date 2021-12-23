04cdb4ea676b0b37008219931394c0de2b44a744
wozza35 <wozza35@hotmail.com>
2021-12-23 19:19:04 +0000

    Fix incorrect filepath

--- lib/magic_links.rb
+++ lib/magic_links.rb
@@ -1,6 +1,6 @@
 require "magic_links/engine"
 require 'rails'
-require 'magic_links/magic_token_authentication'
+require 'magic_links/strategies/magic_token_authentication'
 
 module MagicLinks
   # Your code goes here...
